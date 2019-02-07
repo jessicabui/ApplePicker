5eac79e510b8b1e3904ecebc4c30eb3e2fbdff39
jessicabui <jessicabui97@gmail.com>
2019-02-07 12:18:04 -0500

    Tree Movement

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -23,6 +23,8 @@ public class AppleTree : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
 	}
 }
