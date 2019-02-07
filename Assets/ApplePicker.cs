76ccfd3793b507f091a16c88998bf2f47315b4aa
jessicabui <jessicabui97@gmail.com>
2019-02-07 15:07:43 -0500

    Destroying Basket

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -1,21 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour {
     [Header("Set in Inspector")]
     public GameObject basketPrefab;
     public int numBaskets = 3;
     public float basketBottomY = -14f;
-    public float basketSpacingY = 2f; 
+    public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
 	// Use this for initialization
 	void Start () {
+        basketList = new List<GameObject>();
         for (int i = 0; i < numBaskets; i++) {
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
 
     }
@@ -26,6 +30,11 @@ public class ApplePicker : MonoBehaviour {
         {
             Destroy(tGO);
         }
+
+        int basketIndex = basketList.Count - 1;
+        GameObject tBasketGO = basketList(basketIndex);
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
     }
 
 
